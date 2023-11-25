eee54182e5174e260c2514312eafea806e37dc69
nikkig <60862882+nikkigraybeal@users.noreply.github.com>
2023-11-25 06:24:21 -0800

    fix build error

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,46 +1,44 @@
-import NextAuth from "next-auth/next"
-import GoogleProvider from "next-auth/providers/google"
-import { connectDb } from "../../../../utils/database"
-import User from "@/models/user"
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+import { connectDb } from "../../../../utils/database";
+import User from "@/models/user";
 
-export const authOptions = {
+const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-    callbacks: {
-      async session({session}) {
-        const sessionUser = await User.findOne({ email: session.user.email })
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({ email: session.user.email });
 
-        session.user.id = sessionUser._id
-        console.log("SESSION FROM BE:", session)
-        return session
-      },
-      async signIn({profile}) {
-        console.log("PROFILE:", profile)
-        try {
-          await connectDb()
+      session.user.id = sessionUser._id;
+      console.log("SESSION FROM BE:", session);
+      return session;
+    },
+    async signIn({ profile }) {
+      console.log("PROFILE:", profile);
+      try {
+        await connectDb();
 
-          const userExists = await User.findOne({ email: profile.email })
-          
-          if (!userExists) {
-            await User.create({
-              email: profile.email,
-              name: profile.given_name,
-              image: profile.picture,
-            })
-          }
-          return true
+        const userExists = await User.findOne({ email: profile.email });
 
-        } catch (error) {
-          console.log(error)
-          return false
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            name: profile.given_name,
+            image: profile.picture,
+          });
         }
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
       }
     },
-}
+  },
+});
 
-const handler = NextAuth(authOptions)
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
